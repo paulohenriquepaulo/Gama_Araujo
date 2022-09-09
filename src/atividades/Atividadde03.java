1729e789ac3fb325192dad30fd5c92cb45f9e7ff
paulohenriquepaulo <p.henrique461@gmail.com>
2022-09-09 18:41:38 -0300

    Atividade03

---
+++ src/atividades/Atividadde03.java
@@ -0,0 +1,25 @@
+package atividades;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Atividadde03 {
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+		DecimalFormat format = new DecimalFormat("##.#");
+
+		System.out.print("Informe o valor do salaraio minimo: ");
+		double valorSalarioMinimo = entrada.nextDouble();
+
+		System.out.print("Informe o valor do salaraio do funcionario: ");
+		double valorSalarioFuncionario = entrada.nextDouble();
+
+		double quantidadeSalarioMinimo = valorSalarioFuncionario / valorSalarioMinimo;
+
+		System.out.println("\nO funcionario recebe " + format.format(quantidadeSalarioMinimo) + " salarios minimos!");
+
+		entrada.close();
+	}
+
+}
