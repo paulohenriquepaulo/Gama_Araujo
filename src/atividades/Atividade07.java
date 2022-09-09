c96f5e207d7c9b45fb382cec4e1525bf4be2f988
paulohenriquepaulo <p.henrique461@gmail.com>
2022-09-09 20:14:44 -0300

    Atividade07

---
+++ src/atividades/Atividade07.java
@@ -0,0 +1,25 @@
+package atividades;
+
+import java.util.Scanner;
+
+public class Atividade07 {
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		int soma = 0;
+		
+		System.out.println("Informe o primeiro número: ");
+		int numero1 = entrada.nextInt();
+		
+		System.out.println("Informe o primeiro número: ");
+		int numero2 = entrada.nextInt();
+		
+		for (int i = 0; i < numero2; i++) {
+			soma += numero1;
+		}
+		
+		System.out.println("A multiplicação dos dois números digitados por de somas repetidas é: " + soma);
+		entrada.close();
+	}
+
+}
