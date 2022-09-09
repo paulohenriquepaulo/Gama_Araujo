102f85221c92fc0075f48148b361bf2fa6ba13d1
paulohenriquepaulo <p.henrique461@gmail.com>
2022-09-09 17:42:52 -0300

    Atividade 01

--- src/atividades/Atividade01.java
+++ src/atividades/Atividade01.java
@@ -1,5 +1,16 @@
 package atividades;
 
-public class Atividade01 {
+import java.util.Scanner;
 
+public class Atividade01 {
+		public static void main(String[] args) {
+			
+			Scanner entrada = new Scanner(System.in);
+			
+			System.out.print("Infome o seu nome: ");
+			String nome = entrada.nextLine();
+			
+			System.out.println("Olá " + nome);
+			entrada.close();
+		}
 }
