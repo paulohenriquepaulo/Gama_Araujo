59bc41fc0e53f00bb69f19fbcd76cbbc78f9d2a6
paulohenriquepaulo <p.henrique461@gmail.com>
2022-09-09 19:43:00 -0300

    Atividade05

---
+++ src/atividades/Atividade05.java
@@ -0,0 +1,21 @@
+package atividades;
+
+import java.util.Scanner;
+
+public class Atividade05 {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Informe um número: ");
+		int numero = entrada.nextInt();
+		
+		int antecessor = numero - 1; 
+		int sucessor = numero + 1;
+
+		System.out.println("O antecessor de " + numero + " é " + antecessor +
+				"\nO sucessor de " + numero + " é " + sucessor);
+	}
+
+}
